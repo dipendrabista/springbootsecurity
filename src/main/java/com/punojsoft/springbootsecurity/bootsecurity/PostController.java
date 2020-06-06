a4e7cfa5f7cf56b32629eeafa84e0b927277e071
dipendrabista <punojbista55@gmail.com>
2020-06-06 10:30:19 +0545

    spring boot security setup &secured,preauthorized/postauthorized

---
+++ src/main/java/com/punojsoft/springbootsecurity/bootsecurity/PostController.java
@@ -0,0 +1,31 @@
+package com.punojsoft.springbootsecurity.bootsecurity;
+
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+
+    @Secured("ROLE_USER")
+    @GetMapping
+    public String index() {
+        return "post list";
+    }
+
+    @GetMapping("add")
+    @PreAuthorize(value = "hasRole(ROLE_USER)")
+    public String add() {
+        return "post add";
+    }
+
+    @PostAuthorize(value = "hasAnyRole(ROLE_ADMIN)")
+    @GetMapping("/delete")
+    public String delete() {
+        return "delete post";
+    }
+}
