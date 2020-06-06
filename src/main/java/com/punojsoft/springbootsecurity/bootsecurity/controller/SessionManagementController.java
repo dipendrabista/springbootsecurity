478995f635a1e10640b1e1d7e57c2c28a291f8af
dipendrabista <punojbista55@gmail.com>
2020-06-06 13:17:13 +0545

    session securing using Filter example and expire/invalid session url setting update

--- src/main/java/com/punojsoft/springbootsecurity/bootsecurity/controller/SessionManagementController.java
+++ src/main/java/com/punojsoft/springbootsecurity/bootsecurity/controller/SessionManagementController.java
@@ -1,17 +1,29 @@
 package com.punojsoft.springbootsecurity.bootsecurity.controller;
 
+import com.punojsoft.springbootsecurity.bootsecurity.util.ApiResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @RestController
 public class SessionManagementController {
     @GetMapping("/invalid-session")
-    public String invalidSession() {
-        return "invalid session";
+    public ResponseEntity invalidSession() {
+        ApiResponse apiResponse = new ApiResponse();
+        apiResponse.setDate(new Date());
+        apiResponse.setMessage("Invalid Session");
+        apiResponse.setStatus(440);
+        return ResponseEntity.ok(apiResponse);
     }
 
     @GetMapping("/expired-session")
-    public String expiredSession() {
-        return "expired-session";
+    public ResponseEntity expiredSession() {
+        ApiResponse apiResponse = new ApiResponse();
+        apiResponse.setDate(new Date());
+        apiResponse.setMessage("Expired Session");
+        apiResponse.setStatus(440);
+        return ResponseEntity.ok(apiResponse);
     }
 }
--- src/main/java/com/punojsoft/springbootsecurity/bootsecurity/util/ApiResponse.java
+++ src/main/java/com/punojsoft/springbootsecurity/bootsecurity/util/ApiResponse.java
@@ -1,4 +1,12 @@
 package com.punojsoft.springbootsecurity.bootsecurity.util;
 
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
 public class ApiResponse {
+    private String message;
+    private Date date;
+    private int status;
 }
