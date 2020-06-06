ab2ee7b6a7bf0672ced197eb659a8a30631838c4
dipendrabista <punojbista55@gmail.com>
2020-06-06 11:14:54 +0545

    allowing default url like home to anonymous user

--- src/main/java/com/punojsoft/springbootsecurity/bootsecurity/config/SecurityConfig.java
+++ src/main/java/com/punojsoft/springbootsecurity/bootsecurity/config/SecurityConfig.java
@@ -2,8 +2,16 @@ package com.punojsoft.springbootsecurity.bootsecurity.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/", "/login", "/logout").permitAll();
+        http.formLogin();
+    }
 }
